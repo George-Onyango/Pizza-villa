1951c10ff8722cb23221b948a3d3f76fbcd3eee4
George Onyango <onyangogeorge954@gmail.com>
2020-09-05 21:38:08 +0300

    add required aspect to name and email detail and also add alert message to relay feedback to customer

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
     $("#order").click(function(){
         $("#king").toggle();
     });
+    $("#btn").click(function(event){
+        var client=$("#nameDetail").val();
+        alert('Dear' + 'esteemed' + client + 'we have received the message you have sent. Kindly note that your feedback is highly appreciated');
+        event.preventDefault();
+    });
 });
